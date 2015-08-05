174ca2a2ac4f7d04a9ff9ef2eb2ede64f6908367
Andrea Tarquini <andrea@elearnsecurity.com>
2015-08-05 10:55:04 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,125 @@
+"use strict";
+/*jshint -W083 */
+
+//-------------------------------------
+// MODULE DEPENDENCIES
+//-------------------------------------
+var _ = require("underscore");
+
+//-------------------------------------
+// DEBUGGIN DEPENDENCIES
+//-------------------------------------
+
+// init debug (when the app is initialized with the protecting roles)
+var init_debug = require('debug')('REST-bac:initialization');
+// running debug (when the express app is running)
+var running_debug = require('debug')('REST-bac:running');
+
+//--------------------------------------
+
+
+
+/**
+ * Install protecting routes into the app express application. Roles are defined in the config
+ * @param   {Object} app    The expressjs application to protect with
+ * @param   {Object} config Define the path/method/roles configuration
+ * @param {String} The prefix string of each path to protect
+ */
+function protect_roles(app, config, prefix) {
+
+    for (var path in config) {
+
+        init_debug("Protecting path: " + prefix + path);
+
+        var allow = true;
+        if (config[path].action === "deny")
+            allow = false;
+
+        init_debug("Allow: " + allow);
+
+        for (var method in config[path]) {
+
+            init_debug("Protecting method: " + method);
+
+            if (method != "action") {
+
+                var config_array = config[path][method];
+
+                app[method](prefix + path, (function (allow, conf) {
+
+                    return function (req, resp, next) {
+
+                        running_debug("config roles: " + conf);
+                        running_debug("req roles: " + req.user.roles.toString());
+
+                        var active_roles = _.intersection(conf, req.user.roles);
+
+                        running_debug("active roles: " + active_roles);
+
+
+                        if (active_roles.length > 0) {
+                            running_debug("active roles > 0");
+                            if (allow) {
+                                running_debug("path allowed");
+
+                                req.user.authorized_roles = _.union(req.user.authorized_roles, active_roles);
+
+                            } else {
+                                running_debug("path not allowed");
+                                req.user.authorized_roles = _.difference(req.user.authorized_roles, active_roles);
+                            }
+                        }
+                        next();
+                    };
+
+                })(allow, config_array));
+            }
+        }
+    }
+
+}
+
+/**
+ * The middleware to check if the request contains at least one roles enabled to
+ * fullfill the requesting resource
+ * @param   {Object}   req  the requsest object
+ * @param   {Object} res  the response object
+ * @param   {Function} next next middlware
+ * @returns {Function} the next middleware or 401
+ */
+function check_role_authorization(req, res, next) {
+
+    if (!req.hasOwnProperty("user") || !req.user.hasOwnProperty('authorized_roles') ||
+        !(req.user.authorized_roles instanceof Array) || req.user.authorized_roles.length === 0){
+        
+        running_debug("Unhautorized: Invalid role or path not configured"); 
+        var err = new Error("Unhautorized: Invalid role or path not configured");
+        err.status = 401;
+        return next(err);
+        
+    }
+
+    running_debug("Authorized roles: " + req.user.authorized_roles);
+    return next();
+
+
+}
+
+
+
+
+
+module.exports = function (app, config, prefix) {
+
+    // to protect the app with the path/method/roles defined in config 
+    // we need to create apps middlwares/routes for each path/method
+    // (done by protect_roles) and then to add a middlware to check
+    // if at least one requesting role satisfies the path/method roles
+    // done by (check_role_authorization)
+
+    if (!prefix || typeof prefix != "string") prefix = "";
+
+    protect_roles(app, config, prefix);
+    app.use(prefix + "*", check_role_authorization);
+
+};
---
+++ test/simple.js
@@ -0,0 +1,221 @@
+/* jshint mocha : true */
+/* jshint -W030 */
+"use strict";
+
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    passport = require('passport');
+
+
+var request = require('supertest');
+var chai = require('chai');
+var expect = chai.expect;
+chai.should();
+
+
+var rest_bac = require('../src');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+
+
+var config = {
+
+    "/ua*": {
+        get: ['user', 'admin', 'superadmin'],
+        post: ['admin']
+    },
+
+    "/ua/a": {
+        action: 'deny',
+        get: ['user']
+    },
+
+
+    "/ua/a/u": {
+        get: ['user']
+    },
+
+    "/ua/a/s": {
+        action: 'deny',
+        get: ['user', 'admin']
+    }
+
+};
+
+app.use(function (req, res, next) {
+
+    req.user = {};
+    var auth = req.get('Authorization');
+    if (typeof auth != "undefined")
+        req.user.roles = auth.split(" ");
+    else
+        req.user.roles = [];
+    next();
+});
+
+rest_bac(app, config);
+
+
+app.get('/ua', function (req, res) {
+    res.send("ua");
+});
+
+app.get('/ua/ua', function (req, res) {
+    res.send("ua/ua");
+});
+
+app.get('/ua/a', function (req, res) {
+    res.send("ua/a");
+});
+
+app.get('/ua/a/u', function (req, res) {
+    res.send("ua/a/u");
+});
+
+app.post('/ua/a/u', function (req, res) {
+    res.sendStatus(201);
+});
+
+app.get('/ua/a/s', function (req, res) {
+    res.send("ua/a/s");
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+
+    res.status(err.status || 500);
+    res.json({
+        message: err.message
+    });
+
+});
+
+
+describe('REST-bac simple tests', function () {
+
+    it('Should return a 401 unhautorized when no authentication is provided', function (done) {
+
+        request(app)
+            .get('/ua')
+            .expect(401, done);
+
+    });
+
+    it('Should authorize the "user" role and return 200', function (done) {
+
+        request(app)
+            .get('/ua/ua')
+            .set('Authorization', 'user')
+            .expect(200)
+            .expect("ua/ua", done);
+
+    });
+
+    it('Should authorize the "admin" role and return 200', function (done) {
+
+        request(app)
+            .get('/ua/a')
+            .set('Authorization', 'admin')
+            .expect(200)
+            .expect("ua/a", done);
+
+    });
+
+    it('Should authorize an array of roles containing at least a valid one and return 200', function (done) {
+
+        request(app)
+            .get('/ua/ua')
+            .set('Authorization', 'admin notinrole')
+            .expect(200)
+            .expect("ua/ua", done);
+
+    });
+
+    it('Should deny the "user" role and return 401', function (done) {
+
+        request(app)
+            .get('/ua/a')
+            .set('Authorization', 'user')
+            .expect(401, done);
+
+    });
+
+    it('Should authorize the "user" role and return 200', function (done) {
+
+        request(app)
+            .get('/ua/a/u')
+            .set('Authorization', 'user')
+            .expect(200)
+            .expect("ua/a/u", done);
+
+    });
+
+
+    it('Should deny an array of invalid roles and return 401', function (done) {
+
+        request(app)
+            .get('/ua/a')
+            .set('Authorization', 'user anotherinvalidrole')
+            .expect(401, done);
+
+    });
+
+
+    it('Should authorize the "admin" role even if the req roles contains others invalid roles', function (done) {
+
+        request(app)
+            .get('/ua/a')
+            .set('Authorization', 'admin user')
+            .expect(200)
+            .expect("ua/a", done);
+
+    });
+
+    it('Should authorize the "superadmin" role and return 200', function (done) {
+
+        request(app)
+            .get('/ua/a/s')
+            .set('Authorization', 'superadmin')
+            .expect(200)
+            .expect("ua/a/s", done);
+
+    });
+
+    it('Should deny an array of not "superadmin" roles and return 401', function (done) {
+
+        request(app)
+            .get('/ua/a/s')
+            .set('Authorization', 'admin user')
+            .expect(401, done);
+
+    });
+
+    it('Should authorize the "admin" role and return 201', function (done) {
+
+        request(app)
+            .post('/ua/a/u')
+            .set('Authorization', 'admin')
+            .expect(201, done);
+
+    });
+
+    it('Should deny the "user" role and return 401', function (done) {
+
+        request(app)
+            .post('/ua/a/u')
+            .set('Authorization', 'user')
+            .expect(401, done);
+
+    });
+
+
+
+});
